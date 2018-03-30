2ce3be7d94039573cc9302ffbd3f7a2607bfc8f0
mskian <veerfun23@gmail.com>
2018-03-30 18:51:02 +0530

    🎨  enhancement

--- coinprice.sh
+++ coinprice.sh
@@ -2,68 +2,90 @@
 
 # -----------------------------------------------------------------------------
 # Info:
-#   author:    Santhosh veer <mailto:me@santhoshveer.com>
-#   website:   https://itrendbuzz.com
+#   author:    Santhosh veer
+#   website:   https://santhoshveer.com
 #   file:      coinprice.sh
 #   created:   09.02.2018
-#   version:   0.1
+#   revision:  30.03.2018
+#   version:   0.2
 # -----------------------------------------------------------------------------
 # Requirements:
 #   curl, jq
+#
 # Description:
 #   Coinprice - Cryptocurrency price using Coinmarketcap website api.
-# Usage:
-#   Detailed usage description in the help function.
+#
 # -----------------------------------------------------------------------------
-# Script:
 
+# File name
 SCRIPTNAME=$(basename "$0")
 
+# Help Message
 help(){
+  echo -e "\\n"
   echo "Usage :  $SCRIPTNAME
 
 Run this Script
 
 $ bash coinprice.sh
-+ it will Display top 100 coins ID Name
-+ Enter the coin id name and get its Rankings, Prices, Market cap and More
+- it will Display top 100 coins ID Name
+- Enter the coin id name and get its Rankings, Prices, Market cap and More
 
     Options:
-    -h|help      Display this help message
+    -h   Display this help message
 
-Credits: Using CoinMarketcap API to Fetch the Price Tickers
- "
+"
 }
 
-while getopts "h" flag;
+# Options
+while getopts ":h" flag;
  do
     case "$flag" in
-     h)
+    h)
      help
      exit 0
-     ;;
+    ;;
+  \?)
+    echo "Command Not Found" >&2
+    exit 1
+  ;;
      *)
     esac
   done
+  shift $((OPTIND-1))
 
+# Coinprice ID
+echo -e "\\n"
 echo "TOP 100 Coin ID's"
 
 echo "-----------------"
 
+# cURL Request
 curl -s --request GET \
     --url "https://api.coinmarketcap.com/v1/ticker/" \ | jq -r '["RANK","COIN IDS"], ["--","--------"], (.[] | [.rank, .id]) | @tsv'
 
 
-echo -n "Enter the Coin ID Name or Press Enter to Get all 100 coins INFO: "
+# Get Price Details
+echo -e "\\n"
+echo -n "Enter the Coin ID Name : "
 read -r coinname
 
-  # Change all spaces in the title to "%20" for the URL
-  coinname=${coinname// /%20}
+  # If no URL you will see this Alert message
+  if [[ ! $coinname ]]; then
+    echo -e "\\n"
+    echo -e "\\033[1;31m Error input is Missing \\033[0m \\n"
+    exit 1
+  fi
 
-echo "[+] Fetching the Price Details..."
+# Change all spaces in the title to "%20" for the URL
+Fcoinname=${coinname// /%20}
+
+echo -e "\\n"
+echo -e "\\033[1;32m [+] Fetching the Coin Details... \\033[0m"
 
 
+# cURL Request
 curl -s --request GET \
-    --url "https://api.coinmarketcap.com/v1/ticker/$coinname/" \ | jq
+    --url "https://api.coinmarketcap.com/v1/ticker/$Fcoinname/" \ | jq
 
 exit 0
